1c4a8ae82984889e5d27ea7064f7d1ce5667a863
Zorka <jacobothegreat@gmail.com>
2025-04-01 15:10:57 -0600

    Made it so continuous firing works  - fire_rate is a parameter of the player that controls how fast bullets are shot    modify it by powerups and stuff to change that  - Also fixed a bug where bullets spawned too close to player so player was getting bounced backwards

--- mobs/bullet/bullet.gd
+++ mobs/bullet/bullet.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @export var speed = 800
 
-func start(in_position: Vector2, in_direction: Vector2) -> void:
-	position = in_position + 50*in_direction
+func start(in_position: Vector2, in_direction: Vector2, offset: float) -> void:
+	position = in_position + offset*in_direction
 	velocity = speed*in_direction
 	
 func _physics_process(delta: float) -> void:
--- mobs/player/player.gd
+++ mobs/player/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @export var move_speed = 200
 var Bullet = preload("res://mobs/bullet/bullet.tscn")
+var fire_rate: float = 0.1 # number of seconds between bullets
+var last_shot: float = 0.0
 
 func shoot(shoot_direction: Vector2) -> void:
-	var b = Bullet.instantiate()
-	b.start(global_position, shoot_direction)
-	get_tree().root.add_child(b)
+	var current_time := Time.get_unix_time_from_system()
+	if current_time - last_shot > fire_rate:
+		var b = Bullet.instantiate()
+		b.start(global_position, shoot_direction, 50*(abs(shoot_direction.x) + abs(shoot_direction.y)))
+		get_tree().root.add_child(b)
+		last_shot = current_time
 
 func get_input() -> void:
 	var input_direction: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
