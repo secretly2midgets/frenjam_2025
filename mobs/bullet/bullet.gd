d48705241c12c58e2eaccd5673a0a2c02075cd2c
Zorka <jacobothegreat@gmail.com>
2025-04-02 22:33:24 -0600

    Fixed issue where enemy wasn't dying properly  - Bullet now waits one frame to disappear

--- mobs/bullet/bullet.gd
+++ mobs/bullet/bullet.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 @export var speed = 800
+var die_today: bool = false
 
 # IDEAS FOR BULLET TYPES
 #  - bullet that bounces off walls and lives for a certain amount of time
 #  - triple shot
 #  - bullet that can go through multiple enemies
 #  - fast bullet
-#  - bullet that explodes into multiple bullets
+#  - bullet that explodes into multiple bulslets
 #  - bullet that explodes into AoE in general
 #  - bullet with curved path
 #  - bullet that seeks enemies
@@ -18,6 +19,9 @@ func start(in_position: Vector2, in_direction: Vector2, offset: float) -> void:
 	velocity = speed*in_direction
 	
 func _physics_process(delta: float) -> void:
-	var collision = move_and_collide(velocity * delta)
-	if collision:
+	if !die_today:
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			die_today = true
+	else:
 		queue_free()
