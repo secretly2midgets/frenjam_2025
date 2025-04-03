36bab0db6b4be8a11fa99e0918ee1a8cb392367e
kyanight <kayathies@gmail.com>
2025-04-02 21:14:19 -0600

    Basic sprite addition and rotation
    * Added first dog enemy art and basic character stills
    
    * Connected the sprite to the player and added rotating functionality
    
    * Changed default to texture to skeleton and fixed the scale to match the pixel art size

--- mobs/player/player.gd
+++ mobs/player/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+#Preloading the sprites
+var front_sprite = preload("res://mobs/player/player_front.png")
+var front_side_sprite = preload("res://mobs/player/player_front_side.png")
+var side_sprite = preload("res://mobs/player/player_side.png")
+var back_side_sprite = preload("res://mobs/player/player_back_side.png")
+var back_sprite = preload("res://mobs/player/player_back.png")
+
 @export var move_speed = 200
 var Bullet = preload("res://mobs/bullet/bullet.tscn")
 var fire_rate: float = 0.1 # number of seconds between bullets
@@ -7,6 +14,7 @@ var last_shot: float = 0.0
 
 func shoot(shoot_direction: Vector2) -> void:
 	var current_time := Time.get_unix_time_from_system()
+	change_sprite(shoot_direction)
 	if current_time - last_shot > fire_rate:
 		var b = Bullet.instantiate()
 		b.start(global_position, shoot_direction, 50*(abs(shoot_direction.x) + abs(shoot_direction.y)))
@@ -23,3 +31,18 @@ func get_input() -> void:
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_collide(velocity*delta)
+	
+func change_sprite(shoot_direction: Vector2) -> void:
+	if shoot_direction[0] == 0:
+		if shoot_direction[1] > 0:
+			$Sprite2D.texture = front_sprite
+		elif shoot_direction[1] < 0:
+			$Sprite2D.texture = back_sprite
+	else:
+		if shoot_direction[1] > 0:
+			$Sprite2D.texture = front_side_sprite
+		elif shoot_direction[1] < 0:
+			$Sprite2D.texture = back_side_sprite
+		else:
+			$Sprite2D.texture = side_sprite
+	$Sprite2D.flip_h = shoot_direction[0] < 0
