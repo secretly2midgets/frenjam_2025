1966a3940635d46048a261852aee23bcd6499b37
Zorka <jacobothegreat@gmail.com>
2025-04-02 22:12:35 -0600

    Added handling of collisions with different layers to the default enemy  - bounces off of walls  - dies when shot  - Better behavior needed in several regards as is handling for enemy spawning

--- mobs/enemies/default_enemy.gd
+++ mobs/enemies/default_enemy.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed: float = 200
 var direction: Vector2
@@ -7,4 +7,10 @@ func _ready() -> void:
 	direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
 
 func _physics_process(delta: float) -> void:
-	move_and_collide(direction*speed*delta)
+	var collision = move_and_collide(speed * direction * delta)
+	if collision:
+		var collision_layer = collision.get_collider().get_collision_layer()
+		if collision_layer & 0b1 == 0b1: # wall
+			direction = direction.bounce(collision.get_normal()) 
+		elif collision_layer & 0b100 == 0b100: # bullet
+			queue_free()
