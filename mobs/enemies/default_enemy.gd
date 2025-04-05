83300327c2bd759a994c9d0cf578785e5adb184f
kyanight <kayathies@gmail.com>
2025-04-04 22:37:25 -0600

    Added in art for cyber dog enemy and cat boss. Additionally resolved warning and resized the collission box on the player to match the art

--- mobs/enemies/default_enemy.gd
+++ mobs/enemies/default_enemy.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 # ENEMY IDEAS
 # - Enemy that moves in square around map
-# - Enemy that seeks player directly
+# - Enemy that seeks player directly (undertale dog)
 # - Random walk enemy
 # - Enemy that flees/avoids bullets
-# - Enemy that hides in corner and shoots you
+# - Enemy that hides in corner and shoots you (megaman dog)
 
 @export var speed: float = 200
 var direction: Vector2
@@ -16,7 +16,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(speed * direction * delta)
 	if collision:
-		var collision_layer = collision.get_collider().get_collision_layer()
+		collision_layer = collision.get_collider().get_collision_layer()
 		if collision_layer & 0b1 == 0b1: # wall
 			direction = direction.bounce(collision.get_normal()) 
 		elif collision_layer & 0b100 == 0b100: # bullet
